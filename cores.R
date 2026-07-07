4a882382aea988cd81262e3723aa7fd9913723ee
Pedro de Oliveira Macedo <pedro.macedo@hmv.org.br>
2026-07-07 13:54:43 +0000

    Define cores de sinaleira padrão

---
+++ cores.R
@@ -0,0 +1,5 @@
+# Sinaleira ===================================================================
+cores_vermelho1 <- "#c21d1d"
+cores_laranja1 <- "#ff8c00"
+cores_amarelo1 <- "#e5ff00"
+cores_verde1 <- "#34C759"
