cf0506484e31ef90b22bb9453e800d446edbdf78
Pedro de Oliveira Maedo <pedro.macedo@hmv.org.br>
2026-07-30 12:31:13 +0000

    Cria dados de facilitadores e inclui cores para tabelas

--- 40_facilitadores.R
+++ 40_facilitadores.R
@@ -1,1 +1,43 @@
-# 
+# Consentimento ================================================================
+facilit_seg_andamento_consent <- df |>
+  filter(
+    redcap_event_name == "Baseline (Arm 2: Facilitadores)" &
+      tcle_consentiu_questionarios == "Sim"
+  ) |>
+  reframe(ID = record_id, Consentimento = as.Date(data_preenchi_facili))
+
+
+# Geral =======================================================================
+facilit_seg_andamento_resumo <- df |>
+  filter(
+    record_id %in% facilit_seg_andamento_consent$ID &
+      redcap_event_name %in% c(
+        "Seguimento 3m (Arm 2: Facilitadores)",
+        "Seguimento 6m (Arm 2: Facilitadores)",
+        "Seguimento 9m (Arm 2: Facilitadores)",
+        "Seguimento 12m (Arm 2: Facilitadores)"
+      )) |>
+  select(record_id, redcap_event_name, facilit_segue_estudo, whodas_20_timestamp) |>
+  mutate(
+    redcap_event_name   = paste(str_extract(redcap_event_name, "\\d+"), "meses"),
+    redcap_event_name = factor(
+      redcap_event_name,
+      levels = c("3 meses", "6 meses", "9 meses", "12 meses")
+    ),
+    whodas_20_timestamp = case_when(
+      facilit_segue_estudo == "Sim" & 
+        !is.na(whodas_20_timestamp) ~ as.character(as.Date(whodas_20_timestamp)),
+      facilit_segue_estudo == "Sim" &
+        is.na(whodas_20_timestamp)  ~ "Indisponível",
+      facilit_segue_estudo == "Não" ~ "Saiu do estudo",
+      TRUE ~ NA
+    )
+  ) |>
+  pivot_wider(id_cols = 'record_id', names_from = 'redcap_event_name', 
+              values_from = 'whodas_20_timestamp', names_expand = TRUE) |>
+  mutate(
+    `6 meses`  = if_else(`3 meses` == "Saiu do estudo", "", `3 meses`),
+    `9 meses`  = if_else(`6 meses` == "Saiu do estudo" | `6 meses` == "", "", `9 meses`),
+    `12 meses` = if_else(`9 meses` == "Saiu do estudo" | `9 meses` == "", "", `12 meses`)
+  ) |>
+  rename(ID = record_id)
--- cores.R
+++ cores.R
@@ -1,7 +1,13 @@
 cores_cinza1 <- "grey20"
+cores_tab_cinza1 <- "#4f4f4f"
+cores_tab_cinza2 <- "#b8b6b6"
 
 # Sinaleira ===================================================================
 cores_vermelho1 <- "#c21d1d"
 cores_laranja1 <- "#ff8c00"
 cores_amarelo1 <- "#e5ff00"
 cores_verde1 <- "#34C759"
+
+cores_tab_vermelho1 <- "#f09191"
+cores_tab_amarelo1  <- "#ffe991"
+cores_tab_verde1    <- "#b6e3b6"
