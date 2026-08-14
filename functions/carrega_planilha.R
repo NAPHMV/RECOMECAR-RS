1cf95562285f9fbc97e64275f1c34481c0373faa
Pedro de Oliveira Macedo <pedro.macedo@hmv.org.br>
2026-08-14 12:17:54 +0000

    Adiciona prefixo de pacote em função de carregamento de dados de planilha do Google

--- functions/carrega_planilha.R
+++ functions/carrega_planilha.R
@@ -2,19 +2,19 @@ carrega_planilha <- function(url, aba, token) {
   resp <- httr::GET(
     url,
     query = list(token = token, aba = aba),
-    config(followlocation = TRUE, ssl_verifypeer = TRUE),
-    timeout(30)
+    httr::config(followlocation = TRUE, ssl_verifypeer = TRUE),
+    httr::timeout(30)
   )
   
-  if (status_code(resp) != 200) {
-    stop("Erro HTTP ", status_code(resp), ": ", content(resp, "text", encoding = "UTF-8"))
+  if (httr::status_code(resp) != 200) {
+    stop("Erro HTTP ", httr::status_code(resp), ": ", httr::content(resp, "text", encoding = "UTF-8"))
   }
   
   ct <- httr::headers(resp)$`content-type`
   if (!grepl("application/json", ct, fixed = TRUE)) {
     stop("Resposta não é JSON. Conteúdo recebido: ", 
-         substr(content(resp, "text", encoding = "UTF-8"), 1, 300))
+         substr(httr::content(resp, "text", encoding = "UTF-8"), 1, 300))
   }
   
-  jsonlite::fromJSON(content(resp, "text", encoding = "UTF-8"))
+  jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"))
 }
