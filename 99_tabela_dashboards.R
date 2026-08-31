fc0aa2b9c49d3f35c27d5a04d5569a4b65dae186
Pedro de Oliveira Macedo <pedro.macedo@hmv.org.br>
2026-08-31 17:58:57 +0000

    Atualiza links de relatórios

--- 99_tabela_dashboards.R
+++ 99_tabela_dashboards.R
@@ -1,8 +1,8 @@
 dashs_tab <- tibble::tribble(
-  ~nome,                    ~descricao,                                    ~link,                              ~categoria,
-  "Geral", "Acompanhamento detalhado de todas as etapas do estudo", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_triagem/",  "Geral",
-  "Gestão", "Indicadores direcionados à tomada de decisão", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_fluxo/", "Geral",
-  "Mapeamento", "Mapeamento de triagens realizadas", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_mapeamento/", "Específico",
-  "Manejo de Risco", "Indicadores e evolução dos atendimentos especializados", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_manejo/", "Específico",
-  "Inconsistências", "Monitoramento de dados faltantes e inconsistências na base de dados", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_inconsistencias/", "Específico"
+  ~nome,                    ~descricao,                                    ~link,
+  "Geral", "Acompanhamento detalhado de todas as etapas do estudo", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_triagem/",
+  "Gestão", "Indicadores direcionados à tomada de decisão", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_fluxo/",
+  "Mapeamento", "Mapeamento de triagens realizadas", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_mapeamento/",
+  "Manejo de Risco", "Indicadores e evolução dos atendimentos especializados", "https://nucleodeapoioapesquisa.shinyapps.io/Recomecar_manejo_de_risco/",
+  "Inconsistências", "Monitoramento de dados faltantes e inconsistências na base de dados", "https://nucleodeapoioapesquisa.shinyapps.io/recomecar_inconsistencias/"
 )
